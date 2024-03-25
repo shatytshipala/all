870f24ca27b2cb3ae1b16e96b1fa275072d6d089
shatytshipala <dembetshipala6.com>
2024-03-25 19:59:34 +0200

    email tryagain and agin3

--- config/settings.py
+++ config/settings.py
@@ -139,18 +139,18 @@ ASGI_APPLICATION = "config.asgi.application"
 
 DATABASES = {
     
-   # 'default':dj_database_url.parse(env('DATABASE_URL'))
+    'default':dj_database_url.parse(env('DATABASE_URL'))
       
-        'default':{
+      #  'default':{
        
-      'ENGINE' :'django.db.backends.postgresql',
-        'NAME' :'king',
-        'USER':'postgres',
-       'PASSWORD' : 'Dembe@12',
-       'HOST' :'localhost',
-       'PORT' :'5432',
+     # 'ENGINE' :'django.db.backends.postgresql',
+     #   'NAME' :'king',
+     #   'USER':'postgres',
+     #  'PASSWORD' : 'Dembe@12',
+     #  'HOST' :'localhost',
+     #  'PORT' :'5432',
 
-   }
+  # }
       
      
 
